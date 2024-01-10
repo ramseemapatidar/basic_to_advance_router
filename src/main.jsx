46fdbb787ba9a97679a516c3d4f9c2055a9df0a2
Ankur <ankur@Ankurs-MacBook-Air.local>
2024-01-10 13:47:45 +0530

    ten deleting-records

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import ErrorPage  from './error-page';
 import Contact, { loader as contactLoader,} from "./routes/contact";
 import { loader as rootLoader, action as rootAction, } from "./routes/root";
 import EditContact, { action as editAction, } from "./routes/edit";
-
+import { action as destroyAction } from "./routes/destroy";
   const router = createBrowserRouter([
     {
       path: "/",
@@ -26,6 +26,10 @@ import EditContact, { action as editAction, } from "./routes/edit";
             element: <EditContact />,
             loader: contactLoader,
             action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
           },
       ],
     },
