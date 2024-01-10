807c0959b4abf180ca8b5f2a33ba0729d4f2e842
Ankur <ankur@Ankurs-MacBook-Air.local>
2024-01-10 11:28:32 +0530

    four Loading Data

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import "./index.css";
 import Root from "./routes/root";
 import ErrorPage  from './error-page';
 import Contact from "./routes/contact";
+import { loader as rootLoader } from "./routes/root";
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
       errorElement: <ErrorPage />,
+      loader: rootLoader,
       children: [
         {
           path: "contacts/:contactId",
