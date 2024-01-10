82b6dafb015a556328fbc3ea967dd710e1bd8aac
Ankur <ankur@Ankurs-MacBook-Air.local>
2024-01-10 13:39:25 +0530

    eagth Active Link Styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { Outlet, NavLink, useLoaderData, Form, redirect } from "react-router-dom";
 import { getContacts,createContact } from "../contacts";
 
 export async function loader() {
@@ -7,7 +7,7 @@ export async function loader() {
 }
 export async function action() {
     const contact = await createContact();
-    return { contact };
+    return redirect(`/contacts/${contact.id}/edit`);
 }
 
 
@@ -47,7 +47,13 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink to={`contacts/${contact.id}`} className={({ isActive, isPending }) =>
+                  isActive
+                    ? "active"
+                    : isPending
+                    ? "pending"
+                    : ""
+                }>
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -56,7 +62,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
